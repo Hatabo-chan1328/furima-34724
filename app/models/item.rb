3edfd08a22133a9a62a7e4b4ea6945940ef89282
Hatabo-chan1328 <rihabiri.hanginthere.1135@gmail.com>
2021-07-24 20:41:40 +0900

    商品出品機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,22 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
 
-  validates :name,  presence: true
-  validates :description,  presence: true
-  validates :image, presence: true
-  validates :price, presence: true
+  with_options presence: true do
+   validates :name
+   validates :description
+   validates :image
+   validates :price
+  end
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank" }     
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+   validates :category_id
+   validates :condition_id
+   validates :delivery_fee_id
+   validates :prefecture_id
+   validates :delivery_day_id  
+  end  
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
 
+      it 'ログイン状態のユーザーのみ、商品出品ページへ遷移できること' do
+        @item = FactoryBot.create(:user)
+        expect(@item).to be_valid
+      end
+
+      it '販売価格についての情報が必須であること' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", "Price Half-width number", "Price Out of setting range")
+      end
     end
 
     context '商品出品ページへ遷移できないこと' do
@@ -27,45 +37,51 @@ RSpec.describe Item, type: :model do
       end
 
       it 'category_idが空では登録できないこと' do
-        @item.category_id = ''
+        @item.category_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it 'condition_idが空では登録できないこと' do
-        @item.condition_id = ''
+        @item.condition_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
       it 'delivery_fee_idが空では登録できないこと' do
-        @item.delivery_fee_id = ''
+        @item.delivery_fee_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
 
       it 'prefecture_idが空では登録できないこと' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it 'delivery_day_idが空では登録できないこと' do
-        @item.delivery_day_id = ''
+        @item.delivery_day_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
-      end
-
-      it 'priceが空では登録できないこと' do
-        @item.price = ''
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
       it 'imageが空では登録できないこと' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'priceは、¥300~¥9,999,999の間のみ保存可能であること' do
+        @item.price = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+
+      it 'priceは半角数字のみ保存可能であること' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
       end
     end
   end
