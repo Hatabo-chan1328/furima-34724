71b3f3be018abfe16d63d83aae33bbda9095a374
Hatabo-chan1328 <rihabiri.hanginthere.1135@gmail.com>
2021-07-25 12:39:00 +0900

    商品出品機能の修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe '#create' do
     before do
-      @item = FactoryBot.build(:item)
+      user = FactoryBot.create(:user)
+      @item = FactoryBot.build(:item, user_id: user.id  )
     end
 
-    context '商品出品ページへ遷移できること' do
+    context '商品出品ページへ登録出来ること' do
       it '全ての項目が存在すれば登録できること' do
         expect(@item).to be_valid
       end
@@ -18,7 +19,7 @@ RSpec.describe Item, type: :model do
 
     end
 
-    context '商品出品ページへ遷移できないこと' do
+    context '商品出品ページへ登録出来ないこと' do
       it 'nameが空では登録できないこと' do
         @item.name = ''
         @item.valid?
@@ -67,13 +68,13 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
-      it '販売価格についての情報が必須であること' do
+      it 'priceについての情報が必須であること' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it 'priceは、¥300以下のみ登録可能であること' do
+      it 'priceは、¥300以下では登録出来ないこと' do
         @item.price = 200
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
@@ -89,21 +90,59 @@ RSpec.describe Item, type: :model do
       it 'priceは半角数字のみ出品可能であること' do
         @item.price = '１００００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price Half-width number")
       end
 
       it 'priceは半角英字のみでは出品できないこと' do
         @item.price = 'aaaaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price Half-width number")
       end
 
       it 'priceは半角英字混合では出品できないこと' do
         @item.price = 'aaa111'
         @item.valid?
-        binding.pry
-        expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price Half-width number")
       end
+
+      it 'condition_id:1以外' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it 'category_id:1以外' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'delivery_fee_id:1以外' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
+
+      it 'delivery_day_id:1以外' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+
+      it 'prefecture_id:1以外' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'user_idがないと登録出来ないこと' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
     end
   end
 end
+
+ 
