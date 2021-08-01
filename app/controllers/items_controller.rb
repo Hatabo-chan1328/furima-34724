47591103a48c1c95a381c4bb6b410310f1228e11
Hatabo-chan1328 <rihabiri.hanginthere.1135@gmail.com>
2021-08-01 11:55:50 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :ensure_current_user, only: [:edit, :update]
 
   def index
     @items = Item.order( id: :desc)
@@ -18,9 +19,22 @@ class ItemsController < ApplicationController
     end
   end
 
-    def show
-      @item = Item.find(params[:id])
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
+  end
   
 
 
@@ -29,6 +43,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def ensure_current_user
+    @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
+      redirect_to action: :index
+    end
   end
 
 
